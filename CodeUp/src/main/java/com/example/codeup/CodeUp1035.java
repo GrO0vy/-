108549de1948e17f75ab590c4c71860e2b6d8be9
GrO0vy <lmc2819@naver.com>
2023-04-24 14:47:48 +0900

    feat : CodeUp1033 ~ 1042 문제 풀이 - CodeUp1033 ~ CodeUp1042.java 추가

---
+++ CodeUp/src/main/java/com/example/codeup/CodeUp1035.java
@@ -0,0 +1,14 @@
+package com.example.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1035 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String num = sc.next();
+        int result = Integer.parseInt(num,16);
+
+        System.out.printf("%o",result);
+    }
+}
---
+++ CodeUp/src/main/java/com/example/codeup/CodeUp1036.java
@@ -0,0 +1,14 @@
+package com.example.codeup;
+
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class CodeUp1036 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char cVal = sc.next().charAt(0);
+        int result = cVal;
+        System.out.printf("%d",result);
+    }
+}
