643bae0bf11a0eb3ffc3094e14c9b8fb24abc9c0
GrO0vy <lmc2819@naver.com>
2023-04-24 15:47:25 +0900

    CodeUp 1043 ~ 1051 문제 풀이 - CodeUp1043 ~ 1051.java 추가

---
+++ CodeUp/src/main/java/com/example/codeup/CodeUp1044.java
@@ -0,0 +1,13 @@
+package com.example.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1044 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long num = sc.nextLong();
+
+        System.out.println(++num);
+    }
+}
---
+++ CodeUp/src/main/java/com/example/codeup/CodeUp1047_2.java
@@ -0,0 +1,13 @@
+package com.example.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1047_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        System.out.printf("%d",num<<1);
+    }
+}
