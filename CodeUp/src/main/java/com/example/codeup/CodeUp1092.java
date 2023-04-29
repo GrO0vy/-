356d18308740cb9ee84410108c2abaaf69714e52
GrO0vy <lmc2819@naver.com>
2023-04-29 23:55:06 +0900

    CodeUp1083 ~ CodeUp1092 문제 풀이 - CodeUp1083 ~ CodeUp1092 .java 추가

---
+++ CodeUp/src/main/java/com/example/codeup/CodeUp1092.java
@@ -0,0 +1,13 @@
+package com.example.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1092 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int player1 = sc.nextInt();
+        int player2 = sc.nextInt();
+        int player3 = sc.nextInt();
+    }
+}
